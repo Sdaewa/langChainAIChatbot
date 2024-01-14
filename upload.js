84766724bd234ed33350a1253b33d82036fbefa7
Luis <lar_dev@icloud.com>
2024-01-14 21:59:39 +0100

    upload script

--- upload.js
+++ upload.js
@@ -1,3 +1,4 @@
+import fs from "fs/promises";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { createClient } from "@supabase/supabase-js";
 import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
@@ -5,19 +6,19 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
 // @supabase/supabase-js
 try {
-  const result = await fetch("source.txt");
-  const text = await result.text();
+  const result = await fs.readFile("source.txt", "utf-8");
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 50,
     separators: ["\n\n", "\n", " ", ""], // default setting
   });
 
-  const output = await splitter.createDocuments([text]);
+  const output = await splitter.createDocuments([result]);
 
-  const sbApiKey = process.env.SUPABASE_API_KEY;
-  const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT;
-  const openAIApiKey = process.env.OPENAI_API_KEY;
+  const sbApiKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsZnJzdXpkeHh4am55eWpwcGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUxNjI5NzUsImV4cCI6MjAyMDczODk3NX0.EegEQE-J05Q7WdN12gbhI6JcPFindaiLxSIWieT4gMo";
+  const sbUrl = "https://tlfrsuzdxxxjnyyjppka.supabase.co";
+  const openAIApiKey = "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";
 
   const client = createClient(sbUrl, sbApiKey);
 
